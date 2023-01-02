d832fcce97ed320bf792c68850e5d6ed08148929
Hrudayewar <121657455+Hrudayewar@users.noreply.github.com>
2023-01-02 18:08:53 +0530

    #include<stdio.h> int main() {     int i,english,maths,physics,chemistry,computer;     scanf("%d",&i);     if(i<50)     {         printf("FAILED");     }     else     {         printf("PASSED");     } }

---
+++ Passed_or_Failed.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int i,english,maths,physics,chemistry,computer;
+    scanf("%d",&i);
+    if(i<50)
+    {
+        printf("FAILED");
+    }
+    else
+    {
+        printf("PASSED");
+    }
+}
