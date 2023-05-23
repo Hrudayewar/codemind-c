2a19db4834a42ae4741e14d1e5c9284f3a82523f
Hrudayewar <121657455+Hrudayewar@users.noreply.github.com>
2023-05-23 18:30:59 +0530

    #include<stdio.h> int main ( ) {     int t;     scanf("%d",&t);     while(t--)     {         int n,i,j;         scanf ("%d",&n);         int a[100],t,c=0,s;         for(i=0;i<n;i++)         {            scanf("%d",&a[i]);         }         for(i=0;i<n;i++)         {             for(j=i+1;j<n;j++)             {                 if(a[i]>a[j])                 {                  t=a[i];                 a[i]=a[j];                 a[j]=t;                 c++;                 }             }         }         if(c==0)         {             printf("0 ") ;         }         else         {             s=a[n-1]-a[0];             printf("%d ",s);         }     } }

--- Optimal_Sorting.c
+++ Optimal_Sorting.c
@@ -1,33 +1,40 @@
 #include<stdio.h>
-int main(){
+int main ( )
+{
     int t;
     scanf("%d",&t);
-    while(t--){
+    while(t--)
+    {
         int n,i,j;
-        scanf("%d",&n);
+        scanf ("%d",&n);
         int a[100],t,c=0,s;
-        for(i=0;i<n;i++){
-            scanf("%d",&a[i]);
+        for(i=0;i<n;i++)
+        {
+           scanf("%d",&a[i]);
         }
-        for(i=0;i<n;i++){
-            for(j=i+1;j<n;j++){
-                if(a[j]<a[i]){
-                    t=a[i];
-                    a[i]=a[j];
-                    a[j]=t;
-                    c++;
+        for(i=0;i<n;i++)
+        {
+            for(j=i+1;j<n;j++)
+            {
+                if(a[i]>a[j])
+                {
+                 t=a[i];
+                a[i]=a[j];
+                a[j]=t;
+                c++;   
                 }
             }
         }
-        if(c==0){
+        if(c==0)
+        {
             printf("0
-");
+") ;
         }
-        else{
+        else
+        {
             s=a[n-1]-a[0];
             printf("%d
 ",s);
         }
     }
-    
 }
